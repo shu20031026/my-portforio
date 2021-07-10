b7dd08a8e78b962a9cd4c5ac142e5921c4fb49ee
shu20031026 <shu20031026@gmail.com>
2021-07-11 00:57:11 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "twin.macro";
+
+const hello = () => {
+  return (
+    <div>
+      <button tw="px-4 py-2 border-4 border-green-400 border-solid rounded focus:outline-none">
+        tw prop
+      </button>
+    </div>
+  );
+};
+export default hello;
